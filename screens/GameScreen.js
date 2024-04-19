fc345ab8c6370b961a39dc74add7a4323a17070c
Konstantinos Papadimitriou <papadimitriou@synelixis.com>
2024-04-19 16:15:19 +0300

    new version

---
+++ screens/GameScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  function nextGuessHandler() {}
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+        <PrimaryButton>+</PrimaryButton>
+        <PrimaryButton>-</PrimaryButton>
+      </View>
+    </View>
+  );
+};
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
